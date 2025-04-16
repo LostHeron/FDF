816512600fa2fd106197cbbfa923c2ec25c52e70
Jules Weber <jweber@student.42Lyon.fr>
2025-04-16 16:45:27 +0200

    [FIX] correcting removing or adding minus or plus 2PI were not working

--- src/key_hooks/modify_rot_bonus.c
+++ src/key_hooks/modify_rot_bonus.c
@@ -17,22 +17,22 @@ void	modify_rot_x(t_data *ptr_data)
 {
 	ptr_data->angle_x += PI_OVER_16;
 	if (ptr_data->angle_x > 2 * PI)
-		ptr_data->angle_x += 2 * PI;
+		ptr_data->angle_x -= 2 * PI;
 	set_rot_x(ptr_data);
 }
 
 void	modify_rot_y(t_data *ptr_data)
 {
 	ptr_data->angle_y += PI_OVER_16;
-	if (ptr_data->angle_x > 2 * PI)
-		ptr_data->angle_x += 2 * PI;
+	if (ptr_data->angle_y > 2 * PI)
+		ptr_data->angle_y -= 2 * PI;
 	set_rot_y(ptr_data);
 }
 
 void	modify_rot_z(t_data *ptr_data)
 {
 	ptr_data->angle_z += PI_OVER_16;
-	if (ptr_data->angle_x > 2 * PI)
-		ptr_data->angle_x += 2 * PI;
+	if (ptr_data->angle_z > 2 * PI)
+		ptr_data->angle_z -= 2 * PI;
 	set_rot_z(ptr_data);
 }
--- src/key_hooks/modify_rot_neg_bonus.c
+++ src/key_hooks/modify_rot_neg_bonus.c
@@ -17,22 +17,22 @@ void	modify_rot_x_neg(t_data *ptr_data)
 {
 	ptr_data->angle_x -= PI_OVER_16;
 	if (ptr_data->angle_x < -2 * PI)
-		ptr_data->angle_x -= 2 * PI;
+		ptr_data->angle_x += 2 * PI;
 	set_rot_x(ptr_data);
 }
 
 void	modify_rot_y_neg(t_data *ptr_data)
 {
 	ptr_data->angle_y -= PI_OVER_16;
-	if (ptr_data->angle_x < -2 * PI)
-		ptr_data->angle_x -= 2 * PI;
+	if (ptr_data->angle_y < -2 * PI)
+		ptr_data->angle_y += 2 * PI;
 	set_rot_y(ptr_data);
 }
 
 void	modify_rot_z_neg(t_data *ptr_data)
 {
 	ptr_data->angle_z -= PI_OVER_16;
-	if (ptr_data->angle_x < -2 * PI)
-		ptr_data->angle_x -= 2 * PI;
+	if (ptr_data->angle_z < -2 * PI)
+		ptr_data->angle_z += 2 * PI;
 	set_rot_z(ptr_data);
 }
